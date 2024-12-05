9a131397afdeec6fd0ac464cb97bb4cbaf08b487
RamyaM2005 <ramyareddym023@gmail.com>
2024-12-05 21:31:37 +0530

    Add files via upload

---
+++ IncrimentDecriment.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class ncriment {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        //Pre increment
+        System.out.println(++a);
+        //Post increment
+        System.out.println(b++);
+        //pre decrement
+        System.out.println(--c);
+        //post decrement
+        System.out.println(d--);
+
+
+    }        
+
+}
